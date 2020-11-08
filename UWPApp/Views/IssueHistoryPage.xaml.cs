0e35ad05ed01dc4fb6cf8bea9d1e3468018f0f99
Oscar Keirouz <oscar.keirouz@learnet.se>
2020-11-08 23:05:03 +0100

    .

--- UWPApp/Views/IssueHistoryPage.xaml.cs
+++ UWPApp/Views/IssueHistoryPage.xaml.cs
@@ -46,7 +46,7 @@ namespace UWPApp.Views
         private void LoadActiveIssuesAsync()
         {
             lvActive.ItemsSource = issues
-                .Where(i => i.Status != "New")
+                .Where(i => i.Status != "Pågående")
                 .OrderByDescending(i => i.Created)
                 .Take(SettingsContext.GetMaxItemsCount())
                 .ToList();
